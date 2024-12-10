7eeff5cbd600d21620c9b3b9aebaa6ef03d6829f
mikicvi <88291034+mikicvi@users.noreply.github.com>
2024-12-10 10:50:23 +0000

    ssl with local cert

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # leinster_ttc_booking
+
+Booking automation app for LCC table tennis. No more missing out on booking a slot - automated and optionally recurring.
+
+-   Make directory "certs" at root of the projects
+-   Generate cert with `openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes`
+-   Fill in .env file from `.envsample`
--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const bodyParser = require('body-parser');
 const schedule = require('node-schedule');
 const axios = require('axios');
 const qs = require('qs');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// SSL Certificate
+const key = fs.readFileSync(path.join(__dirname, 'certs', 'key.pem'));
+const cert = fs.readFileSync(path.join(__dirname, 'certs', 'cert.pem'));
 
 let tasks = [];
 const logs = [];
@@ -250,7 +257,12 @@ app.get('/tasks', (req, res) => {
 	res.json(tasks.map((t) => t.key));
 });
 
-// Start server
-app.listen(PORT, () => {
-	log(`Server running on http://localhost:${PORT}`);
+// // Start server
+// app.listen(PORT, () => {
+// 	log(`Server running on http://localhost:${PORT}`);
+// });
+
+// Create HTTPS server
+https.createServer({ key, cert }, app).listen(PORT, () => {
+	console.log(`Server is running on https://localhost:${PORT}`);
 });
